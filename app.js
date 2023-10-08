4ae92daa3383eb2cf794df456de18ff274ebe162
Puncar <0x.puncar@gmail.com>
2023-10-08 16:31:03 -0600

    updated ```

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const dotenv = require('dotenv');
 dotenv.config();
-const { OpenAIApi } = require('openai');
+const openai = require('openai');
 
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
@@ -11,17 +11,13 @@ const apiKey = process.env.OPENAI_API_KEY;
 
 async function promptGPT(prompt) {
     try {
-        const openai = new OpenAIApi({
-            apiKey: apiKey,
-        });
-
-        const response = await openai.chat.completions.create({
-            messages: [{ role: "user", content: prompt }],
-            model: "gpt-3.5-turbo",
+        const response = await openai.completions.create({
+            model: 'gpt-3.5-turbo', // Specify your desired model
+            prompt: prompt,
             max_tokens: 50,
         });
 
-        const output = response.data.choices[0].message.content;
+        const output = response.choices[0].text;
         return output;
     } catch (error) {
         console.error('Error:', error.message);
