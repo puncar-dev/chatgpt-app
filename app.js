14eae117407608a072ac85eff973f156602926f6
Puncar <0x.puncar@gmail.com>
2023-10-08 08:39:07 -0600

    html

--- app.js
+++ app.js
@@ -1,33 +1,49 @@
 const axios = require('axios');
-require('dotenv').config();
+const express = require('express');
+const app = express();
+const dotenv = require('dotenv');
+dotenv.config();
+
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
 
 const apiKey = process.env.OPENAI_API_KEY;
-console.log("API Key:", apiKey);  // Print the API key to the console
-
 
 async function promptGPT(prompt) {
-  try {
-    const response = await axios.post('https://api.openai.com/v1/engines/davinci/completions', {
-      prompt,
-      max_tokens: 50,
-    }, {
-      headers: {
-        'Authorization': `Bearer ${apiKey}`,
-        'Content-Type': 'application/json',
-      },
-    });
+    try {
+        const response = await axios.post('https://api.openai.com/v1/engines/davinci/completions', {
+            prompt,
+            max_tokens: 50,
+        }, {
+            headers: {
+                'Authorization': `Bearer ${apiKey}`,
+                'Content-Type': 'application/json',
+            },
+        });
 
-    const output = response.data.choices[0].text;
-    console.log(output);
-
-    // Implement rate limiting by adding a delay (e.g., 2 seconds) between requests
-    setTimeout(() => {
-      // Make your next request here
-    }, 2000); // Delay in milliseconds
-  } catch (error) {
-    console.error('Error:', error.message);
-  }
+        const output = response.data.choices[0].text;
+        return output;
+    } catch (error) {
+        console.error('Error:', error.message);
+        throw error;
+    }
 }
 
-// Example usage:
-promptGPT('Translate the following English text to French: "Hello, world!"');
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+app.post('/get-completion', async (req, res) => {
+    const prompt = req.body.prompt;
+    try {
+        const output = await promptGPT(prompt);
+        res.json({ output });
+    } catch (error) {
+        res.status(500).json({ error: 'An error occurred' });
+    }
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
