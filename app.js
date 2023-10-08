8df3928293b2e56dfefdd0f3c7fcd632e99a7d9a
Puncar <0x.puncar@gmail.com>
2023-10-08 08:43:56 -0600

    port

--- app.js
+++ app.js
@@ -43,7 +43,7 @@ app.post('/get-completion', async (req, res) => {
     }
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3005;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
