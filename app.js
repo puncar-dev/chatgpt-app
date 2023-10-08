afd95654ffdc35a355efce2ac005f23a77c08017
Puncar <0x.puncar@gmail.com>
2023-10-08 08:22:45 -0600

    dfsdjf

--- app.js
+++ app.js
@@ -1,17 +1,15 @@
-require('dotenv').config(); // Load environment variables from .env file
-
 const axios = require('axios');
+require('dotenv').config();
 
-// Access the API key from the environment variables
 const apiKey = process.env.OPENAI_API_KEY;
-// Print the API key to the console
-console.log("API Key:", apiKey);
-// Define a function to interact with the OpenAI API
+console.log("API Key:", apiKey);  // Print the API key to the console
+
+
 async function promptGPT(prompt) {
   try {
     const response = await axios.post('https://api.openai.com/v1/engines/davinci/completions', {
       prompt,
-      max_tokens: 50, // Adjust as needed
+      max_tokens: 50,
     }, {
       headers: {
         'Authorization': `Bearer ${apiKey}`,
@@ -21,6 +19,11 @@ async function promptGPT(prompt) {
 
     const output = response.data.choices[0].text;
     console.log(output);
+
+    // Implement rate limiting by adding a delay (e.g., 2 seconds) between requests
+    setTimeout(() => {
+      // Make your next request here
+    }, 2000); // Delay in milliseconds
   } catch (error) {
     console.error('Error:', error.message);
   }
